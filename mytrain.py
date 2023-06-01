2374b1fb671d9a7cdc5c1e2812e6bedcd9408e0f
theATM <the_atm@wp.pl>
2023-06-01 20:36:54 +0200

    Update mytrain.py

--- mytrain.py
+++ mytrain.py
@@ -1,18 +1,28 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8s.pt") #YOLO("runs/detect/train/weights/best.pt") #YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolov8s.pt")
+#model = YOLO("runs/detect/train14/weights/best.pt") #YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
+# Use the model:
 
-#%%
-# Use the model
-results = model.train(data="rds-god.yaml", epochs=10,batch=8,freeze=10)  # train the model
-
-#%%
-#results = model.val()  # evaluate model performance on the validation set
+results = model.val(data="./datasets/RSD-YOLO-DOTANA-T/rds-yolo-dotana-t.yaml",save_json=True,plots=True,split='test',coco_annot='./datasets/RSD-YOLO-DOTANA-T/test/instances_test.json')
 
 #%%
-#results = model("./datasets/RSD-GOD-TXT/images/test/",save=True)  # predict on an image
+# train the model:
+# On Pure RSD
+#results = model.train(data="rds-robo.yaml", epochs=200,batch=8,freeze=10,coco_annot='./datasets/RDS-ROBO/valid/instances_val.json', save_json=True, cp_clustering=True)
+# On RSD + DOTA:
+results = model.train(data="./datasets/RSD-YOLO-DOTANA-T/rds-yolo-dotana-t.yaml", epochs=100,batch=8,freeze=0,coco_annot='./datasets/RSD-YOLO-DOTANA-T/valid/instances_val.json',
+                      save_json=True, box_fusion='nms')
+
+# evaluate model performance on the validation set
+results = model.val(save_json=True,plots=True)
+
+# evaluate model performance on the test set
+results = model.val(data="./datasets/RSD-YOLO-DOTANA-T/rds-yolo-dotana-t.yaml",save_json=True,plots=True,split='test',coco_annot='./datasets/RSD-YOLO-DOTANA-T/test/instances_test.json')
+
+# predict on test images
+results = model("./datasets/RSD-YOLO-DOTANA-T/test/images/",data="./datasets/RSD-YOLO-DOTANA-T/rds-yolo-dotana-t.yaml",save=True)
 
 #%%
-#success = model.export(format="onnx")  # export the model to ONNX format
