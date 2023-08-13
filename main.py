46a2c4beb175b012fa363d7279a5da82508d04c5
theATM <the_atm@wp.pl>
2023-08-13 02:07:41 +0200

    Clean the main - make it user friendly and adapt to current changes

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from time import perf_counter, strftime
 ######################################### Main Training Script #######################################################
 
 # Insert dataset path (the yaml file):
-DATASET_YAML_PATH = "../datasets/RSD-YOLO/rds-yolo.yaml"
+DATASET_YAML_PATH = "./datasets/RSD-YOLO/rds-yolo.yaml"
+
+#Predict data path:
+PREDICT_DIR =  '/'.join(DATASET_YAML_PATH.split('/')[:-1]) + "/test/images/"
 
 # Choose the model (config.yaml or pretrained weights.pt or a checkpoint.pt):
 MODEL_CONFIG = 'yolov8s.yaml' # "yolov8n.pt"
@@ -19,6 +22,9 @@ TRAIN = True # starts a trining process
 EVAL =  True # do the model evaluation on val and test sets
 PREDICT = False # Predict on the test images (interference on whole dataset takes time)
 
+# If you want to print coco styled evaluation metrics:
+COCO_EVAL = True  #( the dataset needs to have instances_set.json (a yolified coco format annotations))
+
 ###################
 # Measure time
 start_time = perf_counter()
@@ -28,18 +34,21 @@ model = YOLO(MODEL_CONFIG)
 
 # Train the model:
 if TRAIN:
-    results = model.train(data=DATASET_YAML_PATH, epochs=EPOCHS,batch=BATCH_SIZE,save_json=True, cos_lr=COSINE_LEARNING_RATE)
+    coco_annot = '/'.join(DATASET_YAML_PATH.split('/')[:-1]) + '/valid/instances_val.json' if COCO_EVAL else ''
+    results = model.train(data=DATASET_YAML_PATH, epochs=EPOCHS,batch=BATCH_SIZE,save_json=True, cos_lr=COSINE_LEARNING_RATE, coco_annot=coco_annot)
 
 if EVAL:
     # evaluate model performance on the validation set
     results = model.val(save_json=True,plots=True)
 
     # evaluate model performance on the test set
-    results = model.val(data=DATASET_YAML_PATH,save_json=True,plots=True,split='test')
+    coco_annot = '/'.join(DATASET_YAML_PATH.split('/')[:-1]) + '/test/instances_test.json' if COCO_EVAL else ''
+    results = model.val(data=DATASET_YAML_PATH,save_json=True,plots=True,split='test',coco_annot=coco_annot)
 
 # predict on test images
 if PREDICT:
-    results = model("./datasets/RSD-YOLO-DOTANA-T/test/images/",data="./datasets/RSD-YOLO-DOTANA-T/rds-yolo-dotana-t.yaml",save=True)
+    coco_annot = '/'.join(DATASET_YAML_PATH.split('/')[:-1]) + '/test/instances_test.json' if COCO_EVAL else ''
+    results = model(PREDICT_DIR,data=DATASET_YAML_PATH,save=True,coco_annot=coco_annot)
 
 # Measure time
 end_time = strftime("%H:%M:%S", perf_counter() - start_time)
